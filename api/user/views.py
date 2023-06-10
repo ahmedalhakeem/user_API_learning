71eb8e5e5d97d54a1a9e2f8a64fba8285246f131
ahmedalhakeem <ahmkam2011@gmail.com>
2023-06-11 02:13:17 +0300

    create user

--- api/user/views.py
+++ api/user/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
-
+from rest_framework import views, response, exceptions, permissions
+from .serializers import UserSerializer
+from . import services
 # Create your views here.
+
+class RegisterApi(views.APIView):
+
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        
+        data = serializer.validated_data
+        serializer.instance = services.UserDataClass.create_user(user_dc=data)
+        print(data)
+        return response.Response({"Hello": "world"})
